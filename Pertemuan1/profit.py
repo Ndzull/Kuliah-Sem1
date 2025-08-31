deceecf76182a77834fd1b1b2f5013e13fac7c77
julfunction <119436600+Ndzull@users.noreply.github.com>
2025-08-31 16:56:47 +0700

    Create profit.py

---
+++ Pertemuan1/profit.py
@@ -0,0 +1,7 @@
+# mencari profit
+pembelian=20
+harga_penjualan=30
+profit=harga_penjualan-pembelian
+persenprofit=(100*(profit))//pembelian
+print(f"profitnya: {profit}")
+print(f"persen profitnya: {persenprofit}%")
