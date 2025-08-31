5376bc615921274b58b6bb413f6de018b85ca313
julfunction <119436600+Ndzull@users.noreply.github.com>
2025-08-31 17:10:10 +0700

    Create tabung.py

---
+++ Pertemuan1/tabung.py
@@ -0,0 +1,8 @@
+#cari volume tabung
+t=6
+r=4
+pi=22/7
+volumetabung=pi*(r**2)*t
+luastabung=2*pi*r*(r+t)
+print(f"Volume Tabung:{(round(volumetabung,3))}cm")
+print(f"Luas Tabung:{(round(luastabung,3))}cm")
