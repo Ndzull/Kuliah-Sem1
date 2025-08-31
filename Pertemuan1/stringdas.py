eb21a067e9a91c67bfc3cc9ff15c8f5a97c8727a
julfunction <119436600+Ndzull@users.noreply.github.com>
2025-08-31 17:11:48 +0700

    Create stringdas.py

---
+++ Pertemuan1/stringdas.py
@@ -0,0 +1,7 @@
+# nyoba string dasar
+str1="ijul"
+print(str1[0],str1[2],str1[1:3]) #ju karena indeks 1 sampai 3-1
+print(str1.find("i"))
+#kalo ngga ada yang dicari outputnya -1
+print(str1[-1])
+print(str1[-1],str1[-4],str1[-3:-1])
