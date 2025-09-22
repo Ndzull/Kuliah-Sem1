67f51ad2860fc77080de854c651d844f2f945a86
julfunction <119436600+Ndzull@users.noreply.github.com>
2025-09-22 20:10:46 +0700

    Create cthLatanp.py

---
+++ Pertemuan5/cthLatanp.py
@@ -0,0 +1,12 @@
+print("0123456789012345678901234567")
+print("{0:^5s}{1:<20s}{2:>4s}".format("Rank", "Nama", "IPK"))
+print("{0:^5n}{1:<20s}{2:>4n}".format(1, "Barry Hamdan", 3.98))
+print("{0:^5n}{1:<20s}{2:>4n}".format(2, "Hisam Aaron",3.80))
+print("{0:^5n}{1:<20s}{2:>4n}".format(3, "Rizal Ruth", 3.65))
+
+#anp
+# 0123456789012345678901234567
+# Rank Nama                 IPK
+#   1  Barry Hamdan        3.98
+#   2  Hisam Aaron          3.8
+#   3  Rizal Ruth          3.65
