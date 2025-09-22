d3138a108272689be7da7e6a671d829f6064e98e
julfunction <119436600+Ndzull@users.noreply.github.com>
2025-09-22 20:01:17 +0700

    Create no19anp.py

---
+++ Pertemuan5/no19anp.py
@@ -0,0 +1,6 @@
+#19
+n=float(input("Masukan jumlah detik antara petir dan guntur: "))
+jarak=n/5
+print(f"Jarak dari badai: {round(jarak,2)}") #saya tidak tau kenapa di beberapa compiler/IDE yang saya coba tidak berhasil
+#print("Jarak dari badai: ", round(jarak,2))
+print(f"Jarak dari badai: {jarak:.2f}")
