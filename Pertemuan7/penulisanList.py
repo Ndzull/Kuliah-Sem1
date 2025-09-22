9c286ddd7b0c51e864f086acb02b145952297336
julfunction <119436600+Ndzull@users.noreply.github.com>
2025-09-22 20:16:35 +0700

    Create penulisanList.py

---
+++ Pertemuan7/penulisanList.py
@@ -0,0 +1,22 @@
+team=["Statistka Bisnis", 2020, "ITS"]
+nums=[5,10,4,5]
+words=["spam","ni"]
+wordss=["coba","balik","ini"]
+print(len(team))
+print(max(nums))
+print(min(nums))
+print(sum(nums))
+print(nums.count(5))
+print(nums.index(4))
+words.reverse()
+print(words)
+print(list(reversed(words)))
+
+# 3
+# 10
+# 4
+# 24
+# 2
+# 2
+# ['ni', 'spam']
+# ['spam', 'ni']
