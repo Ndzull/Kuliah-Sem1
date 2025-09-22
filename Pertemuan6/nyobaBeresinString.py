65cef48b51e99ce5b3a7f7f04220e57f13b135aa
julfunction <119436600+Ndzull@users.noreply.github.com>
2025-09-22 20:13:36 +0700

    Create nyobaBeresinString.py

---
+++ Pertemuan6/nyobaBeresinString.py
@@ -0,0 +1,14 @@
+print(" \bnyoba mundur satu")
+print("\fmaksude yo opo")
+print("coba dulu \nbaris baru hore")
+print("iki opo \rxoba dulu")
+print("jadi ini \ttabulasi horizontal")
+print("coba\vtabulasi vertikal")
+
+# nyoba mundur satu
+# maksude yo opo
+# coba dulu 
+# baris baru hore
+# xoba dulu
+# jadi ini 	tabulasi horizontal
+# cobatabulasi vertikal
