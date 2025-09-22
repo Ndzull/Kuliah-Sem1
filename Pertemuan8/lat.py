9e396c0596a4296704406cc6d9c8cf0b4caaddc0
julfunction <119436600+Ndzull@users.noreply.github.com>
2025-09-22 20:26:10 +0700

    Create lat.py

---
+++ Pertemuan8/lat.py
@@ -0,0 +1,11 @@
+judulLagu=("Indonesia Raya", "Lantas", "Backburner")
+penyanyi=("WR. Supratman", "Juicy Lucy", "Niki")
+print("{0:^15s}{1:^20s}".format("Judul Lagu","Penyanyi"))
+print("{0:^15s}{1:^20s}".format(judulLagu[0],penyanyi[0]))
+print("{0:^15s}{1:^20s}".format(judulLagu[1],penyanyi[1]))
+print("{0:^15s}{1:^20s}".format(judulLagu[2],penyanyi[2]))
+
+#   Judul Lagu         Penyanyi      
+# Indonesia Raya    WR. Supratman    
+#     Lantas          Juicy Lucy     
+#   Backburner           Niki        
