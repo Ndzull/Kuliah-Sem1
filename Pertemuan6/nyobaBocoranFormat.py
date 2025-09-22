35b40e7c46480f5b2d78fe693572c9ec757cca4b
julfunction <119436600+Ndzull@users.noreply.github.com>
2025-09-22 20:14:34 +0700

    Create nyobaBocoranFormat.py

---
+++ Pertemuan6/nyobaBocoranFormat.py
@@ -0,0 +1,39 @@
+#coba latihan bocoran dari kls A
+print("{:>100s}".format("TUGAS 1"))
+print("{:^100s}".format("EKSPLORASI DAN VISUALISASI DATA"))
+print("="*100)
+print("{:^100s}".format("PENERAPAN DATA PREPOCESSING DAN DETEKSI OUTLIER"))
+print("{:^100s}".format("PADA INDEKS CYBERSECURITY GLOBAL DAN REGIONAL"))
+print("{:>100s}".format("Oleh"))
+print("{:<90s}{:>0s}".format("Nur Azizah Al-Khaer","2043221084"))
+print("\n")
+print("{:>100s}".format("Dosen"))
+print("{:^100s}".format("Prof. Dr. Wahyu Wibowo, S.Si, M.Si"))
+print("\n")
+print("{:^100s}".format("Program Studi Sarjana Terapan"))
+print("{:^100s}".format("Departemen Statistika Bisnis"))
+print("{:^100s}".format("Fakultas Vokasi"))
+print("{:^100s}".format("Institut Teknologi Sepuluh Nopember"))
+print("{:^100s}".format("Surabaya"))
+print("{:^100s}".format("2025"))
+
+
+#                                                                                              TUGAS 1
+#                                   EKSPLORASI DAN VISUALISASI DATA                                   
+# ====================================================================================================
+#                           PENERAPAN DATA PREPOCESSING DAN DETEKSI OUTLIER                           
+#                            PADA INDEKS CYBERSECURITY GLOBAL DAN REGIONAL                            
+#                                                                                                 Oleh
+# Nur Azizah Al-Khaer                                                                       2043221084
+
+
+#                                                                                                Dosen
+#                                  Prof. Dr. Wahyu Wibowo, S.Si, M.Si                                 
+
+
+#                                    Program Studi Sarjana Terapan                                    
+#                                     Departemen Statistika Bisnis                                    
+#                                           Fakultas Vokasi                                           
+#                                 Institut Teknologi Sepuluh Nopember                                 
+#                                               Surabaya                                              
+#                                                 2025                                                
