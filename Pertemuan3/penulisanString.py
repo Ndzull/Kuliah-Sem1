bf0c9963993c73d65832caaa13b0bdecf0142e4c
julfunction <119436600+Ndzull@users.noreply.github.com>
2025-09-22 19:40:57 +0700

    Create penulisanString.py

---
+++ Pertemuan3/penulisanString.py
@@ -0,0 +1,9 @@
+str="ijul"
+print(len(str))
+print(str.upper())
+print(str.lower())
+print(str.count('i'))
+print(str.capitalize())
+print(str.title())
+print(" ijul".rstrip())
+print(str.replace("i","o"))
