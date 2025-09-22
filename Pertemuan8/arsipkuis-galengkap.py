58d759c18332718199265dafc9eceb8fe9b0c097
julfunction <119436600+Ndzull@users.noreply.github.com>
2025-09-22 20:25:38 +0700

    Create arsipkuis-galengkap.py

---
+++ Pertemuan8/arsipkuis-galengkap.py
@@ -0,0 +1,51 @@
+nama="Alex"
+umur=22
+print(f"Nama saya {nama} dan umur saya adalah {umur}")
+#Nama saya Alex dan umur saya adalah 22
+
+print("nilai ujian: "+str(80+5))
+#nilai ujian: 85
+
+hasil=58.44521
+print(f"{hasil:.2f}%")
+#58.45%
+
+print("hani sedang mengetik",end="")
+print("...")
+#hani sedang mengetik...
+
+a="sub"
+b="20328277"
+c="dhhdejhe"
+print(a,b,c,sep="")
+#sub20328277dhhdejhe
+
+print("File","tugas.txt","tersimpan",sep="")
+#Filetugas.txttersimpan
+
+s1="Mahasiswa{0:s}, ITS{0:s}"
+print(s1.format("Turu"))
+#MahasiswaTuru, ITSTuru
+
+print("Halo,")
+print("Dunia!")
+# Halo,
+# Dunia!
+
+print("Nomor KA\t Tujuan\t Jam\t")
+print("G1234\t\t Halim\t 14.00\t")
+# Nomor KA	 Tujuan	 Jam	
+# G1234		   Halim	 14.00	
+
+judul="Agak Laen"
+print(f"{judul:*^25}")
+#********Agak Laen********
+
+ang=905000000000000000000000
+print(f"Pagu Anggaran: {ang:15d}")
+#Pagu Anggaran: 905000000000000000000000
+
+x=5
+y=3
+print("Jika x={0} dan y={1}, maka x*y={2}".format(x,y,x*y))
+#Jika x=5 dan y=3, maka x*y=15
